95976d11655dc54f1c00dc860a4d5757f6ebeef0
JOSE RAIMUNDO BEZERRA DE CARVALHO <zeraimundo@gmail.com>
2023-12-27 12:41:57 -0300

    Update pxeserver.sh

--- pxeserver.sh
+++ pxeserver.sh
@@ -40,6 +40,19 @@ EOT
 systemctl restart isc-dhcp-server
 
 echo
+echo ------------------- Compartilhando a Internet --------------------
+echo
+
+export INTERFACE=$(ip route | grep default | cut -d ' ' -f 5)
+export IP_ADDRESS=$(ip -4 addr show dev $INTERFACE | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
+sudo sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/' /etc/sysctl.conf
+sudo sysctl -p
+sudo iptables -t nat -A POSTROUTING -o $INTERFACE -j SNAT --to $IP_ADDRESS
+echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
+echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections
+sudo apt-get install iptables-persistent -y
+
+echo
 echo ------------------- Configurando o TFTP Server --------------------
 echo
 
